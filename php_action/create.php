9c9968f57fe35a9f79cfffe0bd592735788ed70c
FabioNog <fabio.carmo@computacao.ufla.br>
2020-02-05 04:57:06 -0300

    Arrumando o require connexao e utilizando o con para acessar o connection ainda apresentando erro

--- php_action/create.php
+++ php_action/create.php
@@ -1,21 +1,21 @@
 <?php
     require_once('../classes/livro.class.php');
-    require_once('../classes/banco.class.php');
+    require_once('../classes/conexao.class.php');
 
-    $con = $this->connection;
-
+    $livro = new livro();
+    $con = new conexao();
+   
     if(isset($_POST['btn-cadastrar'])):
-        $nome_livro = mysqli_escape_string($con,$_POST['nome_livro']);
-        $nacionalidade = mysqli_escape_string($con,$_POST['nacionalidade']);
-        $ano_criacao = mysqli_escape_string($con,$_POST['ano_criacao']);
+        $nome_livro = mysqli_escape_string($con->conection,$_POST['nome_livro']);
+        $nacionalidade = mysqli_escape_string($con->connection,$_POST['nacionalidade']);
+        $ano_criacao = mysqli_escape_string($con->connection,$_POST['ano_criacao']);
         
-        $livro = new livro();
         $livro->setValue('nome_livro',$nome_livro);
         $livro->setValue('nacionalidade',$nacionalidade);
         $livro->setValue('ano_criacao',$ano_criacao);
         
         $sql = $livro->insert($livro);
-        if(mysqli_query($con,$sql)):
+        if(mysqli_query($con->conection,$sql)):
             header('Location: ../index.php?sucesso');
         else:
             header('Location: ../index.php?erro');
