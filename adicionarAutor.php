cccef54775c9bb219da4ead63c5055c10a815864
FabioNog <fabio.carmo@computacao.ufla.br>
2020-02-06 15:48:34 -0300

    Criado Form adicionar Autor

--- adicionarAutor.php
+++ adicionarAutor.php
@@ -6,44 +6,49 @@ include_once('includes/header.php');
 <br>
 <!-- container -->
 <div class="container">
-    <form action="php_action/create.php" method="POST">
+    <form action="php_action/createAutor.php" method="POST">
         <?php
         require_once('classes/livro.class.php');
         require_once('classes/conexao.class.php');
+        require_once('classes/livro.class.php')
 
         ?>
 
         <div class="form-group">
             <div class="col-md-6 offset-md-3">
-                <label>Insira o codigo do livro</label><br>
-                <input type="number" name="liv_cod" id="liv_cod" min="1" max="10000" style="width: 100%">
+                <label>Insira o codigo do autor</label><br>
+                <input type="number" name="aut_cod" id="aut_cod" min="1"  style="width: 100%">
             </div>
-        </div>    
+        </div>
+        
+
+        <div class="form-group">
+            <div class="col-md-6 offset-md-3">
+                <label for="aut_nome"> Nome do Autor</label>
+                <input type="text" name="aut_nome" id="liv_nome" class="form-control " placeholder="Nome do Autor">
+            </div>
+        </div>
+
 
             <div class="form-group">
                 <div class="col-md-6 offset-md-3">
-                    <label for="nome_livro"> Nome do Livro</label>
-                    <input type="text" name="liv_nome" id="liv_nome" class="form-control " placeholder="Nome do Livro">
+                    <label>Insira a data de nascimento</label><br>
+                    <input type="datetime-local" step="59" name="aut_data_nasc" min="1" style="width: 100%">
                 </div>
             </div>
 
             <div class="form-group">
                 <div class="col-md-6 offset-md-3">
-                    <label for="liv_lingua">Nacionalidade:</label>
-                    <select class="form-control" name="liv_lingua">
-                        <option id="liv_lingua"  value="brasileira">Brasileira</option>
-                        <option id="liv_lingua"  value="americana">Americana</option>
-                        <option id="liv_lingua"  value="italiana">Italiana</option>
-                        <option id="liv_lingua"  value="francesa">Francesa</option>
-                        <option id="liv_lingua"  value="francesa">Francesa</option>
-                    </select>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="col-md-6 offset-md-3">
-                    <label>Insira a data</label><br>
-                    <input type="datetime-local" step="59" name="liv_ano" id="liv_ano" min="1" style="width: 100%">
+                    <label>Insira os codigo do livro</label>                    
+                    <select class="form-control" name="liv_cod">
+                        <?php                                
+                            $livro = new livro();
+                            $livro->selectAll($livro);
+                            while($res = $livro->returnDates()):
+                        ?>
+                        <option id="liv_cod"  value=<?php $res->liv_cod?>><?php echo $res->liv_cod?></option>
+                        <?php endwhile;?>
+                        </select>                    
                 </div>
             </div>
             <div class="form-group">
