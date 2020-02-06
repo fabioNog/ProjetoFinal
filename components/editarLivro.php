44b35fc630e25a9d6081c6799ee4c9f81d3b30d4
FabioNog <fabio.carmo@computacao.ufla.br>
2020-02-06 13:02:59 -0300

    adicionando component headerComponentes diferente da header do index

---
+++ components/editarLivro.php
@@ -0,0 +1,60 @@
+<?php
+include_once('../includes/headerComponents.php');
+?>
+
+<br>
+<br>
+<!-- container -->
+<div class="container">
+    <form action="php_action/create.php" method="POST">
+        <?php
+        require_once('../classes/livro.class.php');
+        require_once('../classes/conexao.class.php');
+
+        ?>
+
+        <div class="form-group">
+            <div class="col-md-6 offset-md-3">
+                <label>Insira o codigo do livro</label><br>
+                <input type="number" name="liv_cod" id="liv_cod" min="1" max="10000" style="width: 100%">
+            </div>
+        </div>    
+
+            <div class="form-group">
+                <div class="col-md-6 offset-md-3">
+                    <label for="nome_livro"> Nome do Livro</label>
+                    <input type="text" name="liv_nome" id="liv_nome" class="form-control " placeholder="Nome do Livro">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-md-6 offset-md-3">
+                    <label for="liv_lingua">Nacionalidade:</label>
+                    <select class="form-control" name="liv_lingua">
+                        <option id="liv_lingua"  value="brasileira">Brasileira</option>
+                        <option id="liv_lingua"  value="americana">Americana</option>
+                        <option id="liv_lingua"  value="italiana">Italiana</option>
+                        <option id="liv_lingua"  value="francesa">Francesa</option>
+                        <option id="liv_lingua"  value="francesa">Francesa</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-md-6 offset-md-3">
+                    <label>Insira a data</label><br>
+                    <input type="datetime-local" step="59" name="liv_ano" id="liv_ano" min="1" style="width: 100%">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-6 offset-md-3">
+                    <button type="submit" style="width: 100%" class="btn btn-primary" name="cadastrar" id="cadastrar">Cadastrar</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+<?php
+include_once('../includes/footer.php');
+?>
---
+++ includes/headerComponents.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <link rel="stylesheet" href="../css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="../js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">   
+
+    <!-- Fonts -->
+    <!--  Embed Font -->
+    <link href="https://fonts.googleapis.com/css?family=Poor+Story" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Poor+Story|Spicy+Rice" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="css/Home.css">
+    <title>Livraria</title>
+</head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="../index.php">Livraria SOITIC</a>
+</nav>
+
+<body>
