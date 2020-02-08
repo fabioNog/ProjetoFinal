4485f43578ba57b3f03962a8c1ec4b5f1bd80df1
FabioNog <fabio.carmo@computacao.ufla.br>
2020-02-08 19:39:29 -0300

    Atulizando livro

---
+++ php_action/editLivro.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+    require_once('../classes/livro.class.php');
+    require_once('../classes/conexao.class.php');
+
+    $livro = new livro();
+    $con = new conexao();
+   
+    if(isset($_POST['atualizar'])):
+        $liv_cod = $_POST['liv_cod'];
+        $liv_nome = mysqli_escape_string($con->conection ,$_POST['liv_nome']);
+        $liv_lingua = mysqli_escape_string($con->conection ,$_POST['liv_lingua']);
+        $liv_ano = mysqli_escape_string($con->conection,$_POST['liv_ano']);        
+        $livro->setValue('liv_cod',$liv_cod);
+        $livro->setValue('liv_nome',$liv_nome);
+        $livro->setValue('liv_lingua',$liv_lingua);
+        $livro->setValue('liv_ano',$liv_ano);
+        
+        $sql = $livro->update($livro);
+
+
+        if(mysqli_query($con->conection,$sql)):
+            $_SESSION['mensagem'] = 'Cadastrado com Sucesso';
+            header('Location: ../index.php?');
+        else:
+            $_SESSION['mensagem'] = 'Cadastrado com Sucesso';
+            header('Location: ../index.php?');
+        endif;
+
+    endif;
+?>
